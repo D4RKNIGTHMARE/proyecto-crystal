5d5dcf7ed44c42df060264edda9f0e7c8bdb5d91
D4RKNIGTHMARE <130707667+D4RKNIGTHMARE@users.noreply.github.com>
2024-08-05 13:17:05 -0500

    Logica y css

--- index.js
+++ index.js
@@ -1,35 +1,6 @@
-/*Logica de la seccion de historia de crystal*/ 
-document.addEventListener('DOMContentLoaded', () => {
-    const groupHistory = document.getElementById('group-history');
-
-    function checkScroll() {
-        const rect = groupHistory.getBoundingClientRect();
-        const windowHeight = window.innerHeight;
-
-        if (rect.top < windowHeight && rect.bottom > 0) {
-            groupHistory.classList.add('show');
-        }
-    }
-
-    window.addEventListener('scroll', checkScroll);
-    checkScroll(); // Verifica al cargar la página
-});
-
 /*Logica del video del header*/
-document.addEventListener("DOMContentLoaded", function() {
-    var bgVideo = document.getElementById("bgVideo");
-    
-    setTimeout(function() {
-        var textHeader = document.getElementById("textHeader");
-        var TextHeader = document.getElementById("TextHeader2");
-        textHeader.classList.add("changed-color");
-        TextHeader2.classList.add("change-color-2");
-    }, 28000); 
-
-    
-    bgVideo.addEventListener("ended", function() {
-        bgImage.classList.add("hidden");
-    });
+document.getElementById('bgVideo').addEventListener('ended', function() {
+  document.getElementById('banner-content').style.opacity = 1;
 });
 
 /*Logica de los Integrantes*/
@@ -71,3 +42,20 @@ function openEvent(url) {
 }
 
 showSlide(currentSlide);
+
+/*Logica de la seccion de historia de crystal
+document.addEventListener('DOMContentLoaded', () => {
+  const groupHistory = document.getElementById('group-history');
+
+  function checkScroll() {
+      const rect = groupHistory.getBoundingClientRect();
+      const windowHeight = window.innerHeight;
+
+      if (rect.top < windowHeight && rect.bottom > 0) {
+          groupHistory.classList.add('show');
+      }
+  }
+
+  window.addEventListener('scroll', checkScroll);
+  checkScroll(); // Verifica al cargar la página
+});*/
